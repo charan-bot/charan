ff80409cb667e98b3d3ab578ce535ecc7ea5eecd
MKJ <MKJ@MKJ-PC>
2019-08-08 19:33:13 +0530

    final

--- commit2/src/commit2/CommitAttempt2.java
+++ commit2/src/commit2/CommitAttempt2.java
@@ -4,7 +4,7 @@ public class CommitAttempt2 {
 
 	public static void main(String[] args) {
 		System.out.println("test commit 2");
-		System.out.println("test");
+		//System.out.println("test");
 
 	}
 
