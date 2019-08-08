3955ee25a68f2fc4d4bb612f5063d61b5860d7e6
MKJ <MKJ@MKJ-PC>
2019-08-08 17:17:35 +0530

    3rd attempt

---
+++ commit2/src/commit2/CommitAttempt2.java
@@ -0,0 +1,30 @@
+package commit2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CommitAttempt2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\MKJ\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.navigate().refresh();
+        driver.manage().window().maximize();
+      
+        WebElement Element= driver.findElement(By.tagName("Email"));
+        js.executeScript("arguments[0].scrollintoView();",Element);
+        driver.switchTo().frame("a077aa5e");
+        driver.findElement(By.xpath("/html/body/a/img")).click();
+        driver.quit();
+
+	}
+
+}
